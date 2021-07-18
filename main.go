e3469eb00048a29e68012085b95305c4d4b306ac
Marcel Schramm <marceloschr@googlemail.com>
2021-07-18 15:36:05 +0200

    Improve logging and documentation

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -19,41 +20,56 @@ func main() {
 	}()
 	signal.Notify(onExit, syscall.SIGINT)
 	for {
-		buttonValue, buttonError := os.ReadFile("/sys/class/gpio/gpio17/value")
-		if buttonError == nil {
-			if buttonValue[0] == '1' {
-				fmt.Println("BUTTON WAS PRESSED")
-				barcode, barcodeError := readBarcode()
-				if barcodeError == nil {
-					fmt.Println("The barcode is", barcode)
-				} else {
-					fmt.Println(barcodeError)	
-					//TODO: was wenn barcode nicht gelesen wrden konnte?
-				}
-				
-			}
-		} else {
-			fmt.Println(buttonError)
-			os.Exit(-1)
+		err := logic()
+		if err != nil {
+			log.Printf("Fatal error: %s\n", err)
+			break
 		}
-
 	}
 }
 
-func readBarcode() (string,error) {
-	imageTempPath := "/run/user/1000/barcode.png"
-	captureError := exec.Command("raspistill", "--timeout", "1" , "--output", imageTempPath, "--nopreview", "--quality", "10").Run()
-	
+// logic contains the application logic requried for the workflow of buying
+// a drink. An error is only returned, if a non recoverable situation has
+// been encountered.
+func logic() error {
+	buttonValue, buttonError := os.ReadFile("/sys/class/gpio/gpio17/value")
+	if buttonError != nil {
+		return buttonError
+	}
+
+	//Values can be 0 and 1.
+	if buttonValue[0] == '1' {
+		log.Println("BUTTON WAS PRESSED")
+		barcode, barcodeError := readBarcode()
+		if barcodeError == nil {
+			log.Println("The barcode is", barcode)
+		} else {
+			//Valid case, as the picture can be blurry or there's no barcode present.
+			log.Println("Error reading barcode:", barcodeError)
+		}
+	}
+
+	return nil
+}
+
+// readBarcode takes an image via raspistill and tries parsing it with zbarimg.
+func readBarcode() (string, error) {
+	imageTempPath := "/run/user/1000/barcode.jpg"
+
+	raspistillCommand := exec.Command("raspistill", "--timeout", "1", "--encoding", "jpg", "--output", imageTempPath, "--nopreview", "--quality", "10", "--rotation", "180")
+	log.Println("Executing:", raspistillCommand)
+	captureError := raspistillCommand.Run()
 	if captureError != nil {
-		return "", captureError
+		return "", fmt.Errorf("error capturing image: %s", captureError)
 	}
 
 	zBarCommand := exec.Command("zbarimg", "-Sdisable", "-Sean13.enable", "--raw", "--quiet", imageTempPath)
+	log.Println("Executing:", zBarCommand)
 	var barcodeBuffer bytes.Buffer
 	zBarCommand.Stdout = &barcodeBuffer
 	zBarError := zBarCommand.Run()
 	if zBarError != nil {
-		return "", zBarError
+		return "", fmt.Errorf("error parsing image:%s", zBarError)
 	}
 	return barcodeBuffer.String(), nil
 }
